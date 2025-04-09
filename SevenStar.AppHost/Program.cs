8565bb8df7bc14993effafac5cb06273539908b9
DESKTOP-1IO9F8H\mkore <mkore8123@gmail.com>
2025-04-09 20:02:27 +0800

    添加 Aspire 架構下的初始化專案

---
+++ SevenStar.AppHost/Program.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.SevenStar_ApiService>("apiservice");
+
+builder.AddProject<Projects.SevenStar_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
