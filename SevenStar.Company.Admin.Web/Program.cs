61df12a86475dc2737c49bc273cfdf4809f56ce4
mkore8123 <mkore8123@gmail.com>
2025-07-05 22:14:49 +0800

    no message

--- SevenStar.Company.Admin.Web/Program.cs
+++ SevenStar.Company.Admin.Web/Program.cs
@@ -1,19 +1,34 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SevenStar.Company.Admin.Web;
+using SevenStar.Company.Admin.Web.Components;
+using Syncfusion.Blazor;
 
-// 建立 WebAssemblyHostBuilder，會自動載入 appsettings.json 與環境設定
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCeUx/WmFZfVtgdVVMZFRbRXBPMyBoS35Rc0VkWX1fcHBdR2ZZVEJ+VEFd");
 
-// 註冊根組件 <App>，並把它掛到 wwwroot/index.html 裡的 <div id="app"></div>
-builder.RootComponents.Add<App>("#app");
-// 註冊 <HeadOutlet>，讓組件可以用 <PageTitle> 或 <HeadContent> 自動把 <title> 與 <meta> 注入 <head>
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebApplication.CreateBuilder(args);
 
-// 註冊 HttpClient 到 DI 容器，讓組件可以 @inject HttpClient。
-// 預設 BaseAddress 是 index.html 所在位址，用於呼叫相同主機上的 API 或靜態檔案。
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents()
+    .AddInteractiveServerComponents();
 
-// 建立並啟動 Blazor WebAssembly 應用程式
-// 這會在瀏覽器下載 .dll 與 WASM runtime，並開始執行 <App>
-await builder.Build().RunAsync();
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
